a88efdc5bbfc863c43e4944d5ec4e53aedb99e97
Valentin Pachkevich <vel2253741@gmail.com>
2025-11-08 22:42:16 +0300

    Initial commit: Project structure with Docker, PostgreSQL, MySQL, Kafka

---
+++ src/test/java/com/pashcevich/data_unifier/DataUnifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pashcevich.data_unifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataUnifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
