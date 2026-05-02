8d85fbde485cf83f26eca53f93d3202d480122e4
PashkevichValetin <PashkevitchValentinJava@yandex.ru>
2026-05-02 11:40:43 +0300

    fix: add JsonDeserializer.VALUE_DEFAULT_TYPE to Kafka consumer config

--- src/main/java/com/pashcevich/data_unifier/config/KafkaConsumerConfig.java
+++ src/main/java/com/pashcevich/data_unifier/config/KafkaConsumerConfig.java
@@ -1,33 +1,39 @@
 package com.pashcevich.data_unifier.config;
-
 import com.pashcevich.data_unifier.adapter.kafka.producer.dto.UnifiedCustomerDto;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.ssl.SslBundles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.util.backoff.FixedBackOff;
-
 import java.util.Map;
 
-@Slf4j
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, UnifiedCustomerDto> consumerFactory(
-            KafkaProperties kafkaProperties) {
+            KafkaProperties kafkaProperties,
+            ObjectProvider<SslBundles> sslBundles) { // Добавляем этот параметр
 
-        Map<String, Object> props = kafkaProperties.buildConsumerProperties();
+        Map<String, Object> props = kafkaProperties.buildConsumerProperties(sslBundles.getIfAvailable());
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        props.put(JsonDeserializer.TYPE_MAPPINGS,
-                "unifiedCustomerDto:com.pashcevich.data_unifier.adapter.kafka.producer.dto.UnifiedCustomerDto");
+        props.put(JsonDeserializer.TYPE_MAPPINGS, "unifiedCustomerDto:" + UnifiedCustomerDto.class.getName());
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, UnifiedCustomerDto.class);
 
         return new DefaultKafkaConsumerFactory<>(props);
     }
@@ -35,22 +41,10 @@ public class KafkaConsumerConfig {
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, UnifiedCustomerDto> kafkaListenerContainerFactory(
             ConsumerFactory<String, UnifiedCustomerDto> consumerFactory) {
-
         ConcurrentKafkaListenerContainerFactory<String, UnifiedCustomerDto> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
-
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
-                (record, exception) -> {
-                    log.error("Failed to process message from topic {} partition {} offset {}: {}",
-                            record.topic(), record.partition(), record.offset(),
-                            exception.getMessage(), exception);
-                },
-                new FixedBackOff(1000L, 3)
-        );
-        factory.setCommonErrorHandler(errorHandler);
         factory.setConcurrency(3);
-
         return factory;
     }
 }
